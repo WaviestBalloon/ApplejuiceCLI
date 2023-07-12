04c9927e2035d01cd357e123a7ea37ff39cbb742
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-07-12 04:35:42 +0100

    improved terminal colours

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@ set -e
 echo "Hello! This is the Applejuice CLI installer, keep in mind that this is still in development and may not work as expected.\nFurthermore, this installer will require sudo privileges to install the CLI to /usr/local/bin."
 echo ""
 
+if [[ $EUID -eq 0 ]]; then
+	echo "This script is restricted to run as a non-root user only. Do not run as root or sudo."
+	exit 1
+fi
+
 echo "Checking if cargo is usable..."
 if ! command -v cargo &> /dev/null
 then
 	echo "Cargo could not be found, please install Rust from https://rustup.rs/."
-	exit
+	exit 1
 fi
 echo "^ Done!"
 
@@ -27,4 +32,4 @@ echo "---------------------------"
 echo "^ Done!"
 
 echo ""
-echo "Done! You can now run the Applejuice CLI by typing 'applejuicecli' in your terminal."
+echo "Success: You can now run the Applejuice CLI by typing 'applejuicecli' in your terminal."
--- src/utils/terminal.rs
+++ src/utils/terminal.rs
@@ -1,10 +1,10 @@
 use std::process;
 
 pub fn error(message: String) {
-	println!("\x1b[93m[X] {}\x1b[0m", message);
+	println!("\x1b[91m\x1b[1m[ x ]\x1b[0m \x1b[91m{}\x1b[0m", message);
 	process::exit(1);
 }
 
 pub fn warning(message: String) {
-	println!("\x1b[93m[!] {}\x1b[0m", message);
+	println!("\x1b[93m\x1b[1m[ ! ]\x1b[0m {}", message);
 }
