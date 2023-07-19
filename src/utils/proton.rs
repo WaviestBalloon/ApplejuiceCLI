7c9a3c190e8c4520904bf1be1a4b0a8723161562
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-07-19 14:35:17 +0100

    Fix spell, potental_path -> potential_path

--- src/utils/proton.rs
+++ src/utils/proton.rs
@@ -3,16 +3,16 @@ use crate::utils::terminal::*;
 use serde_json;
 
 pub fn discover_proton_directory() -> serde_json::Value { // Try to automatically find where Proton is installed to
-	let potental_path = format!("{}/.steam/steam/steamapps/common/", env!("HOME"));
+	let potential_path = format!("{}/.steam/steam/steamapps/common/", env!("HOME"));
 	let mut installations = serde_json::json!({
 		"proton_installations": { }
 	});
 
-	match fs::read_dir(potental_path.clone()) {
+	match fs::read_dir(potential_path.clone()) {
 		Ok(_) => {
-			status(format!("Found Steam directory at '{}'", potental_path));
+			status(format!("Found Steam directory at '{}'", potential_path));
 
-			for entry in fs::read_dir(potental_path.clone()).unwrap() {
+			for entry in fs::read_dir(potential_path.clone()).unwrap() {
 				let unwrapped_entry = entry.unwrap();
 				let path = unwrapped_entry.path();
 				let fsname = path.file_name().unwrap().to_str().unwrap();
@@ -26,7 +26,7 @@ pub fn discover_proton_directory() -> serde_json::Value { // Try to automaticall
 			return installations;
 		},
 		Err(_) => {
-			warning(format!("Failed to find the Steam directory at '{}'", potental_path));
+			warning(format!("Failed to find the Steam directory at '{}'", potential_path));
 			return "null".parse().unwrap();
 		}
 	}
