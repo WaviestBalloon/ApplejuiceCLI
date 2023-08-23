de1a3b6a959cd75b2f9a8e6fe7908d2ae3cbaac2
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-08-23 15:45:37 +0100

    Remove enumerate from args.rs which is not needed

--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,5 +1,5 @@
 pub fn get_param_value(command_vector: Vec<Vec<(String, String)>>, value_to_find: &str) -> String {
-	for (index, command) in command_vector.iter().enumerate() {
+	for command in command_vector.iter() {
 		if command[0].0 == value_to_find.to_string() {
 			return command[0].1.to_string();
 		}
