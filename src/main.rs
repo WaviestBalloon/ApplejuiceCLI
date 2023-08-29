3904cdfb12106f2e7a52fe118217afa81345c670
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-08-29 16:20:38 +0100

    Remove debug code from main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ mod args; // Import modules which act as a handler for certain command parameter
 use crate::utils::{terminal::*, *};
 
 fn main() {
-	utils::configuration::test_balls();
-
 	if env::consts::OS != "linux" {
 		error(format!("Applejuice is a Linux-only application and is not designed to be ran on any operating system other than a Linux-based system.\nYour OS identifies itself as {}!\n{}", env::consts::OS, if env::consts::OS == "windows" { "Since you are using Windows, consider using Bloxstrap: https://github.com/pizzaboxer/bloxstrap/\n" } else { "" }));
 	}
