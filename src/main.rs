3030696fb935573b741499bb6aea7096992754f1
Daniel Conley <himself@danii.dev>
2023-09-01 12:06:09 -0400

    Make OS check compile time

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ mod utils; // Import utilities that are not necessarily commands
 mod args; // Import modules which act as a handler for certain command parameters
 use crate::utils::{terminal::*, *};
 
+#[cfg(not(target_os = "linux"))]
+compile_error!("Applejuice is a Linux-only application and is not designed to be ran on any operating system other than a Linux-based system.");
+
+#[cfg(target_os = "windows")]
+compile_error!("Since you are compiling for Windows, consider using Bloxstrap: https://github.com/pizzaboxer/bloxstrap/");
+
 fn main() {
-	if env::consts::OS != "linux" {
-		error(format!("Applejuice is a Linux-only application and is not designed to be ran on any operating system other than a Linux-based system.\nYour OS identifies itself as {}!\n{}", env::consts::OS, if env::consts::OS == "windows" { "Since you are using Windows, consider using Bloxstrap: https://github.com/pizzaboxer/bloxstrap/\n" } else { "" }));
-	}
 	let args: Vec<String> = env::args().collect();
 	if !setup::confirm_applejuice_data_folder_existence() && args[1] != "init" { // Initialisation warning
 		warning("Applejuice has not been initialised yet! Attempting to initialise...");
