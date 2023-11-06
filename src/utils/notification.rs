febbd5292da61732ac6eacc135393e55c2234de5
licktheroom <77417158+licktheroom@users.noreply.github.com>
2023-11-06 19:39:37 +0000

    prevent stack overflow due to uncontrolled recursion

--- src/utils/notification.rs
+++ src/utils/notification.rs
@@ -16,6 +16,11 @@ pub fn create_notification(icon: &str, expire_time: &str, title: &str, body: &st
 		Err(errmsg) => { // Do not quit or panic here, since it's a non-critical error
 			warning!("Failed to create notification, raw: '{}'\nError: {}", icon, errmsg);
 			
+			if icon.is_empty() {
+				warning!("Failed to create notification twice; stopping creation.");
+				return;
+			}
+
 			if errmsg.to_string().contains("No such file or directory (os error 2)") { // Fallback to default/no icon if we detect a missing file error
 				warning!("Assuming a asset was missing; falling back to no icon...");
 				create_notification("", expire_time, title, body);
