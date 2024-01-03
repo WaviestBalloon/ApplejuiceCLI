dcf51807c4cdde6af0184bb3a1f97d477e59410e
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2024-01-03 18:59:00 +0000

    Fix placeholder causing notification icons not showing

--- src/utils/notification.rs
+++ src/utils/notification.rs
@@ -4,7 +4,7 @@ use crate::utils::terminal::*;
 pub fn create_notification(icon: &str, expire_time: &str, title: &str, body: &str) {
 	let output = process::Command::new("notify-send")
 		.arg("--app-name=Applejuice")
-		.arg(format!("--icon={}", "aaaaa"))
+		.arg(format!("--icon={}", icon))
 		.arg("--urgency=normal")
 		.arg(format!("--expire-time={}", expire_time))
 		.arg(title)
